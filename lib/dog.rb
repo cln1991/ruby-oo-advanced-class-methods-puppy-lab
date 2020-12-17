02672cc3db5770c6d2ea6466d12815cedb485b44
Christopher <christophernavac@gmail.com>
2020-12-17 16:28:48 -0500

    commit tree a7bb80a3225e76126281e33f384dc94a1d3f0da0

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,29 @@
-# Add your code here
+class Dog
+
+    attr_accessor :name
+  
+    @@all = []
+  
+    def initialize(dog_name)
+      @name = dog_name
+      save
+    end
+  
+    def self.all
+      @@all.each do |dog|
+        puts dog.name
+      end
+    end
+  
+    def self.clear_all
+      @@all.clear
+    end
+
+    def self.print_all
+        Dog.all
+    end
+
+    def save
+        @@all << self
+    end
+  end
